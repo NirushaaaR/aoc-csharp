a4e4b19e14606b2583b27858223bd4058e359dcb
IceNirush <nirush@digio.co.th>
2023-12-10 16:07:39 +0700

    Day9

---
+++ 2023/Day9/Day9.cs
@@ -0,0 +1,82 @@
+namespace AOC.Y2023;
+
+class Day9(string inputName = "input.txt") : BaseDay(2023, 9, inputName)
+{
+	public override double Solve1()
+	{
+		double sum = 0;
+		foreach (string input in inputs)
+		{
+			List<int[]> sequences = [input.Split(' ').Select(int.Parse).ToArray()];
+			while (true)
+			{
+				List<int> differences = [];
+				bool allDiffIsZeros = true;
+				var nums = sequences.Last();
+				for (int i = 1; i < sequences.Last().Length; i++)
+				{
+					int diff = nums[i] - nums[i - 1];
+					differences.Add(diff);
+					if (diff != 0)
+					{
+						allDiffIsZeros = false;
+					}
+				}
+				if (allDiffIsZeros) {
+					break;
+				}
+				sequences.Add([.. differences]);
+			}
+
+			var sequencesArr = sequences.ToArray();
+
+			int nextNum = 0;
+			for (int i = sequencesArr.Length - 1; i >= 0; i--)
+			{
+				nextNum = sequencesArr[i][^1] + nextNum;
+			}
+
+			sum += nextNum;
+		}
+		return sum;
+	}
+
+	public override double Solve2()
+	{
+		double sum = 0;
+		foreach (string input in inputs)
+		{
+			List<int[]> sequences = [input.Split(' ').Select(int.Parse).ToArray()];
+			while (true)
+			{
+				List<int> differences = [];
+				bool allDiffIsZeros = true;
+				var nums = sequences.Last();
+				for (int i = 1; i < sequences.Last().Length; i++)
+				{
+					int diff = nums[i] - nums[i - 1];
+					differences.Add(diff);
+					if (diff != 0)
+					{
+						allDiffIsZeros = false;
+					}
+				}
+				if (allDiffIsZeros) {
+					break;
+				}
+				sequences.Add([.. differences]);
+			}
+
+			var sequencesArr = sequences.ToArray();
+
+			int nextNum = 0;
+			for (int i = sequencesArr.Length - 1; i >= 0; i--)
+			{
+				nextNum = sequencesArr[i][0] - nextNum;
+			}
+
+			sum += nextNum;
+		}
+		return sum;
+	}
+}
