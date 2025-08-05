9317b58374fa08e0d63092bfa4aa586014adbb5a
IceNirush <nirush@digio.co.th>
2025-08-05 23:07:06 +0700

    add memory usage

--- Solver.cs
+++ Solver.cs
@@ -6,19 +6,26 @@ class Solver
 {
     public static void Solve(BaseDay day)
     {
+        
         Stopwatch stopWatch = new();
         stopWatch.Start();
+        double kbAtExecution = GC.GetTotalMemory(false) / 1024;
         var answer = day.Solve1();
+        double kbAfter = GC.GetTotalMemory(false) / 1024;
         stopWatch.Stop();
+
         TimeSpan ts = stopWatch.Elapsed;
         Console.Write($"Day{day.day} Part1: {answer}, ");
-        Console.WriteLine($"RunTime {ts.TotalMilliseconds} ms");
+        Console.WriteLine($"RunTime {ts.TotalMilliseconds} ms, MemoryUsage {kbAfter - kbAtExecution:F2} kB;");
 
         stopWatch.Restart();
+        kbAtExecution = GC.GetTotalMemory(false) / 1024;
         answer = day.Solve2();
+        kbAfter = GC.GetTotalMemory(false) / 1024;
         stopWatch.Stop();
+
         ts = stopWatch.Elapsed;
         Console.Write($"Day{day.day} Part2: {answer}, ");
-        Console.WriteLine($"RunTime {ts.TotalMilliseconds} ms");
+        Console.WriteLine($"RunTime {ts.TotalMilliseconds} ms, MemoryUsage {kbAfter - kbAtExecution:F2} kB;");
     }
 }
