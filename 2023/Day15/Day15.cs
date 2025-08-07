284aec26e5a93d8f7ccbc8bf2d74a7a5ea12a61e
NirushaaaR <nirushsrijaimoon@gmail.com>
2025-08-07 20:05:05 +0700

    solve day 15

--- 2023/Day15/Day15.cs
+++ 2023/Day15/Day15.cs
@@ -24,8 +24,70 @@ class Day15(string inputName = "input.txt") : BaseDay(2023, 15, inputName)
 		return sum;
 	}
 
+	static void PrintBox(Dictionary<int, List<string>> boxes)
+	{
+		foreach (var (key, value) in boxes)
+		{
+			Console.WriteLine($"{key}: {string.Join(',', value)}");
+		}
+		Console.WriteLine();
+	}
+
+	static int CalculateFocusingPower(Dictionary<int, List<string>> boxes, Dictionary<string, int> lensFocalLength)
+	{
+		int power = 0;
+		foreach (var (boxIndex, box) in boxes)
+		{
+			for (int slot = 0; slot < box.Count; slot++)
+			{
+				string len = box[slot];
+				int focalLength = lensFocalLength[len];
+				power += (1 + boxIndex) * (slot + 1) * focalLength;
+			}
+		}
+		return power;
+	}
+
 	public override double Solve2()
 	{
-		return 0;
+		Dictionary<int, List<string>> boxes = [];
+		Dictionary<string, int> lensFocalLength = [];
+
+		foreach (var input in inputs[0].Split(","))
+		{
+			int signIndex = input.IndexOf('=');
+			if (signIndex != -1)
+			{
+				// = opertaion
+				int focalLength = Convert.ToInt32(input[(signIndex + 1)..]);
+				string label = input[..signIndex];
+				int boxIndex = Hash(label);
+
+				if (!lensFocalLength.ContainsKey(label))
+				{
+					// add new one to the boxes
+					if (!boxes.TryGetValue(boxIndex, out List<string> box))
+					{
+						box = [];
+						boxes[boxIndex] = box;
+					}
+					box.Add(label);
+				}
+
+				lensFocalLength[label] = focalLength;
+			}
+			else
+			{
+				// - operation
+				string label = input[..^1];
+				if (lensFocalLength.Remove(label))
+				{
+					// if exists, remove the box
+					int boxIndex = Hash(label);
+					boxes[boxIndex].Remove(label);
+				}
+			}
+		}
+		return CalculateFocusingPower(boxes, lensFocalLength);
 	}
 }
